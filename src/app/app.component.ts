e433e4958033dc117a3d76dbf6362333a7de0445
gayatri <82455051+gayatripawar2001@users.noreply.github.com>
2024-04-09 20:06:46 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Hello omkaaaaaaaaar!</h1>`,
+  template: `<h1>Hello World Im Gayatri Pawar From Milky way galaxy!!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
